aa9afff9200b5768a4d2ffe97c5a0bd486a3a49d
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-22 03:16:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ def reverse_each_word(string)
  
 
 string_array = []
- string 
-# = string.split 
-string.each {|word| []<< word }
+ string = string.split 
+string.each {|word| [].reverse << word }
 
 end
 
